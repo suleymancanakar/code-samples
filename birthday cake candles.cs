b464a4514d736a433369e650eb13bf7c354a5830
suleymancanakar <31029818+suleymancanakar@users.noreply.github.com>
2020-01-26 14:31:24 +0300

    Add files via upload

---
+++ birthday cake candles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the birthdayCakeCandles function below.
+     */
+    static int birthdayCakeCandles(int n, int[] ar) {
+        /*
+         * Write your code here.
+         */
+        var tallestOne = ar.Max();
+        var candleCount = ar.Count(x => x == tallestOne);
+        return candleCount;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = birthdayCakeCandles(n, ar);
+
+        tw.WriteLine(result);
+
+        tw.Flush();
+        tw.Close();
+    }
+}
---
+++ encryption.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the encryption function below.
+    static string encryption(string s) {
+    
+            s = s.Replace(" ", String.Empty);
+            int length = s.Length;
+            double karekok = Math.Sqrt(length);
+            int min = (int)karekok;
+            int max = min == karekok ? min :  min + 1;
+            string result = "";
+
+            for (int i = 0; i < max; i++)
+            {
+                string str = "";
+                for (int j = i; j < length; j = j + max)
+                {
+                    str += s[j];
+                }
+                result += str + " ";
+            }
+            return result.TrimEnd();
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = encryption(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
